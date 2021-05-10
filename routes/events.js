c1b7af72b2fcc8bc8bf904f500e138cbc4bc103d
Bishyan Kar <bishyankar99@gmail.com>
2021-05-10 22:18:28 +0530

    auth middleware attached

--- routes/events.js
+++ routes/events.js
@@ -1,18 +1,19 @@
+/* eslint-disable prettier/prettier */
 var express = require('express');
 var eventContoller = require('../controllers/eventController');
 var router = express.Router();
+var Authenticate = require('../middlewares/auth');
 
-router.get('/', eventContoller.getAllEvents);
-router.get('/query', eventContoller.getEvents);
-router.get('/:eventCode', eventContoller.getEventByCode);
-// router.get('/price', eventContoller.getEventByPrice);
-// router.get('/disc', eventContoller.getEventByDiscount);
-// router.get('/status', eventContoller.getEventByStatus);
-// router.get('/category', eventContoller.getEventByCategory);
+//user and admin
+router.get('/', Authenticate.isLoggedIn, eventContoller.getAllEvents);
+router.get('/query',  Authenticate.isLoggedIn, eventContoller.getEvents);
+router.get('/:eventCode', Authenticate.isLoggedIn, eventContoller.getEventByCode);
 
-router.post('/add', eventContoller.addEvent);
-// router.post('/register', eventContoller.registerUser);
-router.put('/edit', eventContoller.updateEvent);
-router.delete('/delete', eventContoller.delete);
+
+//admin
+router.post('/add', Authenticate.isLoggedIn,  Authenticate.isAdmin, eventContoller.addEvent);
+
+router.put('/edit', Authenticate.isLoggedIn,  Authenticate.isAdmin, eventContoller.updateEvent);
+router.delete('/delete', Authenticate.isLoggedIn,  Authenticate.isAdmin, eventContoller.delete);
 
 module.exports = router;
